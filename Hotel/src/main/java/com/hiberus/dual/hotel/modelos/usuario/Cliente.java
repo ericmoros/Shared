dff0bf767d421f3bec78f0c0cd285dda5b297588
Eric Moros Pérez <emoros@hiberus.com>
2018-09-24 11:59:04 +0200

    [Hotel] Correcion de usuario y  extra de habitacion

--- Hotel/src/main/java/com/hiberus/dual/hotel/modelos/usuario/Cliente.java
+++ Hotel/src/main/java/com/hiberus/dual/hotel/modelos/usuario/Cliente.java
@@ -2,11 +2,14 @@ package com.hiberus.dual.hotel.modelos.usuario;
 
 import java.util.ArrayList;
 
+import javax.persistence.Entity;
+
 import com.hiberus.dual.hotel.modelos.hotel.Reserva;
 
 /**
  * cliente
  */
+@Entity
 public class Cliente extends Usuario{
     
     public Cliente(String dni, String nombre, ArrayList<Reserva> reservas) {
