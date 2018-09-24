3e19a598805c0aa85966a45db4fa6081c6f0c0fc
alopezv <alopezv@hiberus.com>
2018-09-24 11:49:09 +0200

    [hotel] Servicios añadidos

---
+++ Hotel/src/main/java/com/hiberus/dual/hotel/servicios/IServicioServicio.java
@@ -0,0 +1,21 @@
+package com.hiberus.dual.hotel.servicios;
+
+import java.util.ArrayList;
+
+import com.hiberus.dual.hotel.modelos.hotel.Servicio;
+
+/**
+ * IServicioServicio
+ */
+public interface IServicioServicio {
+
+    ArrayList<Servicio> getAll();
+
+    Servicio get(Long id);
+
+    void add(Servicio servicio);
+
+    void update(Servicio servicio);
+
+    void remove(Long id);
+}
