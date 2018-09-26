cc32e42734c7b8e1abafdad280af34e0f4cb6ef7
alopezv <alopezv@hiberus.com>
2018-09-26 13:15:00 +0200

    [hotel] extras

--- Hotel/src/test/java/com/hiberus/dual/hotel/modelos/habitacion/ExtraTest.java
+++ Hotel/src/test/java/com/hiberus/dual/hotel/modelos/habitacion/ExtraTest.java
@@ -42,30 +42,30 @@ public class ExtraTest {
 		Assert.assertEquals(new BigDecimal("1").setScale(2, BigDecimal.ROUND_UP), rExtra.getPrecio());
 	}
 
-//	@Test
-//	public void testUpdate() {
-//		Extra extra = new Extra();
-//		Optional<Extra> oRExtra = null;
-//		Extra rExtra = null;
-//
-//		extra.setId(1L);
-//		extra.setNombre("Testing");
-//		extra.setPrecio(new BigDecimal("1").setScale(2, BigDecimal.ROUND_UP));
-//		eR.save(extra);
-//
-//		extra.setId(2L);
-//		extra.setNombre("Testing2");
-//		extra.setPrecio(new BigDecimal("2").setScale(2, BigDecimal.ROUND_UP));
-//		eR.save(extra);
-//
-//		oRExtra = eR.findById(extra.getId());
-//		if (oRExtra.isPresent())
-//			rExtra = oRExtra.get();
-//		Assert.assertNotNull(extra.getId());
-//		Assert.assertEquals((Long) 1L, rExtra.getId());
-//		Assert.assertEquals("Testing", rExtra.getNombre());
-//		Assert.assertEquals(new BigDecimal("1").setScale(2, BigDecimal.ROUND_UP), rExtra.getPrecio());
-//	}
+	@Test
+	public void testUpdate() {
+		Extra extra = new Extra();
+		Optional<Extra> oRExtra = null;
+		Extra rExtra = null;
+
+		extra.setId(1L);
+		extra.setNombre("Testing");
+		extra.setPrecio(new BigDecimal("1").setScale(2, BigDecimal.ROUND_UP));
+		eR.save(extra);
+		
+		extra.setId(2L);
+		extra.setNombre("Testing2");
+		extra.setPrecio(new BigDecimal("2").setScale(2, BigDecimal.ROUND_UP));
+		eR.save(extra);
+
+		oRExtra = eR.findById(extra.getId());
+		if (oRExtra.isPresent())
+			rExtra = oRExtra.get();
+		Assert.assertNotNull(extra.getId());
+		Assert.assertEquals((Long) 2L, rExtra.getId());
+		Assert.assertEquals("Testing2", rExtra.getNombre());
+		Assert.assertEquals(new BigDecimal("2").setScale(2, BigDecimal.ROUND_UP), rExtra.getPrecio());
+	}
 
 	
 }
