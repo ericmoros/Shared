8add3d215321062f3dfca7abd9e2a1824d0e50f2
alopezv <alopezv@hiberus.com>
2018-09-26 09:52:05 +0200

    [hotel] reformar test

--- Hotel/src/test/java/com/hiberus/dual/hotel/modelos/habitacion/Extra.java
+++ Hotel/src/test/java/com/hiberus/dual/hotel/modelos/habitacion/Extra.java
@@ -1,4 +1,4 @@
-package com.hiberus.dual.hotel.modelos;
+package com.hiberus.dual.hotel.modelos.habitacion;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +11,6 @@ public class Extra {
 
 	@Test
 	public void contextLoads() {
-	}
-
+    }
+    
 }
