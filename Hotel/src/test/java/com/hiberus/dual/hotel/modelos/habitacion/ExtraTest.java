e7eb27c5982a6e7088c88cc8bd79828fab4e0ed0
Eric Moros Pérez <emoros@hiberus.com>
2018-09-27 13:09:49 +0200

    [Hotel] ExtraTest método de testReadAll

--- Hotel/src/test/java/com/hiberus/dual/hotel/modelos/habitacion/ExtraTest.java
+++ Hotel/src/test/java/com/hiberus/dual/hotel/modelos/habitacion/ExtraTest.java
@@ -1,6 +1,9 @@
 package com.hiberus.dual.hotel.modelos.habitacion;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import com.hiberus.dual.hotel.repositorios.ExtraRepositorio;
@@ -17,27 +20,52 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ExtraTest {
 	@Autowired
 	private ExtraRepositorio eR;
-
+	
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	
 	@Test
 	public void testRead() {
 		Extra extra = null;
 		Extra extraOld = null;
 		Optional<Extra> oExtra = null;
-
+		
 		extra = new Extra(1L, "Testing", new BigDecimal("2").setScale(2, BigDecimal.ROUND_UP));
 		extraOld = new Extra(extra);
 		eR.save(extra);
 		
 		oExtra = eR.findById(extra.getId());
-
+		
 		if (oExtra.isPresent()) extra = oExtra.get();
 		Assert.assertEquals(extraOld, extra);
 	}
 	
+	@Test
+	public void testReadAll() { // El test individual no funciona.
+		List<Extra> extras = null;
+		List<Extra> extrasOld = null;
+
+		extras = new ArrayList<Extra>(
+			Arrays.asList(
+				new Extra[] {
+					new Extra(4L, "Testing", new BigDecimal("2.5").setScale(2, BigDecimal.ROUND_UP)),
+					new Extra(5L, "Testing2", new BigDecimal("2.9").setScale(2, BigDecimal.ROUND_UP)),
+					new Extra(6L, "Testing3", new BigDecimal("2.2").setScale(2, BigDecimal.ROUND_UP)),
+					new Extra(7L, "Testing4", new BigDecimal("2.0").setScale(2, BigDecimal.ROUND_UP)),
+					new Extra(8L, "Testing5", new BigDecimal("2").setScale(2, BigDecimal.ROUND_UP)),
+				}
+			)
+		);
+		extrasOld = new ArrayList<Extra>(extras);
+		eR.saveAll(extras);
+
+		extras = (List<Extra>) eR.findAll();
+		Assert.assertEquals(extrasOld, extras);
+		
+	}
+
 	@Test
 	public void testCreate() {
 		Extra extra = null;
@@ -85,5 +113,6 @@ public class ExtraTest {
 		oExtra = eR.findById(extra.getId());
 		Assert.assertTrue(!oExtra.isPresent());
 	}
+
 
 }
